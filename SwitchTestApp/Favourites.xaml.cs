41be7f8baac19894ed9f64ae4c66550aa8df9176
jodi319 <jodi319@gmail.com>
2017-05-25 16:17:21 +0200

    Implemented switched  (check box) in Xamarin.Forms. Can programmatically set a switch, can detect when a switch is set via UI.

--- SwitchTestApp/Favourites.xaml.cs
+++ SwitchTestApp/Favourites.xaml.cs
@@ -12,87 +12,19 @@ namespace SwitchTestApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Favourites : ContentPage
     {
-        public Favourites(bool isOn1, bool isOn2, bool isOn3)
-        {
-            isOn1 = true;
-            isOn2 = true;
-            isOn3 = true;
-        }
-
-        public bool isOn1 { get; set; }
-        public bool isOn2 { get; set; }
-        public bool isOn3 { get; set; }
         public Favourites()
         {
             InitializeComponent();
+            //DisplayAlert("Alert", "Start " , "OK");
+            //switch02.IsToggled = true;
+            //switch03.IsToggled = true;
+            //switch04.IsToggled = true;
         }
 
-        //protected override void OnAppearing()
-        //{
-        //    isOn1 = true;
-        //    isOn2 = false;
-        //    isOn3 = true;
-        //}
-            
-        //void isOn1(object sender, ToggledEventArgs e)
-        //{
-        //    //lbldisp.Text = "Switch Status is " + e.Value.ToString();
-        //    DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        //}
-
-        private void Switch2_OnToggled(object sender, ToggledEventArgs e)
+        private void Switches_OnToggled(object sender, ToggledEventArgs e)
         {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch3_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch4_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch5_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch6_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch7_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch8_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch9_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch10_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch11_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
-        }
-
-        private void Switch12_OnToggled(object sender, ToggledEventArgs e)
-        {
-            DisplayAlert("Alert", "Switch Status is " + e.Value.ToString(), "OK");
+            //DisplayAlert("Alert", switch03.ClassId + " Status is " + e.Value.ToString(), "OK");
+            LblStatus.Text = LblStatus.Text + ", " + ((Switch)sender).ClassId + " = " + e.Value.ToString();
         }
     }
 }
